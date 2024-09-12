2ef7dd8a4c7058e9f9c9a3e7b31850bc4096b7b1
ahammedhridoy <developerhridoy@gmail.com>
2024-09-12 21:26:42 +0600

    product details ui fixed

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -18,24 +18,41 @@ const ProductDetails = ({
 
   return (
     <div className="p-4 w-full pb-5">
-      <div className="w-full flex flex-col md:flex-row justify-center md:justify-between items-center md:items-start text-center md:text-left">
+      <div className="w-full flex flex-col md:flex-row justify-center md:justify-center items-center md:items-start text-center md:text-left">
         <div className="flex flex-col items-center">
           <div className="flex flex-wrap justify-center space-x-4 mb-4">
-            {labels.map((label : any, index : number) => (
-              <div key={index} className="bg-[#0a2847] w-[72px] h-[72px] md:w-[92px] md:h-[92px] rounded-full flex justify-center items-center">
-                <div className='bg-white w-[64px] h-[64px] md:w-[84px] md:h-[84px] rounded-full flex justify-center items-center text-center'>
+            {labels.map((label: any, index: number) => (
+              <div
+                key={index}
+                className="bg-[#0a2847] w-[72px] h-[72px] md:w-[92px] md:h-[92px] rounded-full flex justify-center items-center"
+              >
+                <div className="bg-white w-[64px] h-[64px] md:w-[84px] md:h-[84px] rounded-full flex justify-center items-center text-center">
                   <div className="bg-[#0a2847] w-[56px] h-[56px] md:w-[76px] md:h-[76px] rounded-full flex justify-center items-center font-black overflow">
-                    {label.length < 8 ? label : <div className='flex-col justify-center items-center text-center flex'>
-                      {
-                        label === 'NO PRESERVATIVE' ? <>
-                        <p className='text-[10px] md:text-[12px] text-center flex justify-center'>No</p>
-                        <p className='text-[6px] md:text-[8px] text-center flex justify-center'>PRESERVATIVE</p>
-                        </> : <>
-                        <p className='text-[6px] md:text-[8px] text-center flex justify-center'>{getFirstString(label)}</p>
-                        <p className='text-[10px] md:text-[12px] text-center flex justify-center'>{label.split(" ")[label.split(" ").length - 1]}</p>
-                        </>
-                      }
-                    </div>}
+                    {label.length < 8 ? (
+                      label
+                    ) : (
+                      <div className="flex-col justify-center items-center text-center flex">
+                        {label === "NO PRESERVATIVE" ? (
+                          <>
+                            <p className="text-[10px] md:text-[12px] text-center flex justify-center">
+                              No
+                            </p>
+                            <p className="text-[6px] md:text-[8px] text-center flex justify-center">
+                              PRESERVATIVE
+                            </p>
+                          </>
+                        ) : (
+                          <>
+                            <p className="text-[6px] md:text-[8px] text-center flex justify-center">
+                              {getFirstString(label)}
+                            </p>
+                            <p className="text-[10px] md:text-[12px] text-center flex justify-center">
+                              {label.split(" ")[label.split(" ").length - 1]}
+                            </p>
+                          </>
+                        )}
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
@@ -43,23 +60,30 @@ const ProductDetails = ({
           </div>
           {labelFooter ? (
             <div>
-              <h6 className='bg-white text-black py-2 px-4 mt-3 rounded-full text-[10px] md:text-[12px] text-center'>
-                <b>Note-</b>{labelFooter}
+              <h6 className="bg-white text-black py-2 px-4 mt-3 rounded-full text-[10px] md:text-[12px] text-center">
+                <b>Note-</b>
+                {labelFooter}
               </h6>
             </div>
-          ) : null }
+          ) : null}
         </div>
         <div className="text-center md:text-left md:ml-10 mt-4 md:mt-0 flex flex-col items-center md:items-start">
           <div className="text-xs md:text-sm text-black mb-1">
-            <h4><strong className='text-black font-black'>Shelf Life</strong></h4>
+            <h4>
+              <strong className="text-black font-black">Shelf Life</strong>
+            </h4>
             <h5>{life}</h5>
           </div>
           <div className="text-xs md:text-sm text-black mb-1">
-            <h4><strong className='text-black font-black'>Storage</strong></h4>
+            <h4>
+              <strong className="text-black font-black">Storage</strong>
+            </h4>
             <h5>{storage}</h5>
           </div>
           <div className="text-xs md:text-sm text-black mb-1">
-            <h4><strong className='text-black font-black'>Ingredient</strong></h4>
+            <h4>
+              <strong className="text-black font-black">Ingredient</strong>
+            </h4>
             <h5>{ingredient}</h5>
           </div>
         </div>
