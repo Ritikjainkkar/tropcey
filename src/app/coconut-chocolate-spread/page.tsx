ec5bf021b666786fc0c8409964c32bd27225c05c
Ashik Ahammed Hridoy <anonymous@Ashiks-MacBook-Air.local>
2026-06-03 00:05:43 +0600

    BG Issue Fixed

--- src/app/coconut-chocolate-spread/page.tsx
+++ src/app/coconut-chocolate-spread/page.tsx
@@ -51,7 +51,7 @@ const Page: React.FC<{ searchParams: { [key: string]: string } }> = ({
         leftSrc={chip.background.left}
         RightSrc={chip.background.right}
       /> */}
-      <div className="relative bg-baked-chips-bg-left bg-baked-chips-bg-right text-white flex flex-col lg:flex-row items-start min-h-[100vh] w-full z-10 pb-5 lg:pt-[100px] pt-[40px] lg:bg-[url('/images/background/spread-bg-img.jpg')] bg-[url('/images/background/chocolate-mb-bg.png')] lg:bg-[length:100%_100%]">
+      <div className="relative bg-baked-chips-bg-left bg-baked-chips-bg-right text-white flex flex-col lg:flex-row items-start min-h-[100vh] w-full z-10 pb-5 lg:pt-[100px] pt-[40px] lg:bg-[url('/images/background/spread-bg-img.jpg')] bg-[url('/images/background/chocolate-mb-bg.png')] lg:bg-[length:100%_100%] bg-right bg-no-repeat bg-cover">
         <LeftContainer
           imgSrc={spread.imgUri}
           brandingSubtitle={spread.brandingSubtitle}
