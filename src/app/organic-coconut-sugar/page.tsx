7f1583350eaa0428fa08dc2b51f3c4ff2af33837
Ashik Ahammed Hridoy <anonymous@Ashiks-MacBook-Air.local>
2026-06-02 23:09:43 +0600

    Page bg issue fix

--- src/app/organic-coconut-sugar/page.tsx
+++ src/app/organic-coconut-sugar/page.tsx
@@ -42,7 +42,7 @@ const Page: React.FC<{ searchParams: { [key: string]: string } }> = ({
         leftSrc={chip.background.left}
         RightSrc={chip.background.right}
       /> */}
-      <div className="relative bg-baked-chips-bg-left bg-baked-chips-bg-right text-white flex flex-col lg:flex-row items-start min-h-[100vh] w-full z-10 pb-5 lg:pt-[100px] pt-[40px] lg:bg-[url('/images/background/sugar-bg.jpg')] bg-[url('/images/background/sugar-mb-bg.png')] bg-[length:100%_100%] bg-center">
+      <div className="relative bg-baked-chips-bg-left bg-baked-chips-bg-right text-white flex flex-col lg:flex-row items-start min-h-[100vh] w-full z-10 pb-5 lg:pt-[100px] pt-[40px] lg:bg-[url('/images/background/sugar-bg.jpg')] bg-[url('/images/background/sugar-mb-bg.png')] lg:bg-[length:100%_100%] bg-center  bg-cover">
         <LeftContainer
           imgSrc={sugar.imgUri}
           brandingSubtitle={sugar.brandingSubtitle}
